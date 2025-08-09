6bbe44e4d30874a98a7d5aa71985aed2b1fe5748
juantox64@gmail.com <juan@DESKTOP-R58JPDQ.>
2025-08-09 15:14:56 -0500

    updating everything to date

--- food/views.py
+++ food/views.py
@@ -44,7 +44,25 @@ def create_item(request):
     return render(request, 'food/item-form.html', {'form':form} )
 
 
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request,'food/item-form.html',{'form':form, 'item':item})
+    #form is returned for context but the item data also needs to be edited thats why i also pass it into the context so that its passed to the form to be able to be edited 
 
+def delete_item(request, id):
+    #id on top is us getting the id of the item to than be able to use an object to access that specfic item with the same id thats why id = id 
+    item = Item.objects.get(id=id)
+
+    if request.method == 'POST': 
+        item.delete()
+        return redirect('food:index')
+    #this is for a template making sure that is the item we want to delete and we have to pass in item as the context so it shows up from the database and shows us which item we want to delete 
+    return render(request,'food/item-delete.html', {'item':item})
 
 
 
