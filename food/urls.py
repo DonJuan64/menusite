20d7c90d33807093269ea26f8ba8e535d2bf8303
Don_Juan <Juantox64@gmail.com>
2025-09-05 19:02:32 -0500

    Cleanup: ignore cache/db + update views, urls, and base template

--- food/urls.py
+++ food/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/', views.item, name='item'),
     #add items 
-    path('add/',views.create_item,name='create_item')
+    path('add/',views.create_item,name='create_item'),
+    #edit items
+    path('update/<int:id>/', views.update_item, name='update_item'),
     
 ]
 
